b002de79d761cc62740b28551208e32e6ac2db79
Francisco Giordano <frangio.1@gmail.com>
2016-06-22 12:05:51 -0300

    Integra IO 3

--- DispositivosInOut/leds/ControladorLED.v
+++ DispositivosInOut/leds/ControladorLED.v
@@ -19,14 +19,18 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+  //  ControladorLED CntrlLed(.clk(clk), .reset(reset), .we(device0in[18]), .reg_sel(device0in[17:16]), .cs(device0cs), .in(device0in[15:0]), .out(device0out));
 
 module ControladorLED(
+    input clk, reset, we, cs,
+    input [1:0] reg_sel,
     input [15:0] in,
-    input clk, reset,
-    input we,
-    output reg [7:0] Led
+    output [15:0] out
     );
 
+    assign out = {8'b0, Led};
+
+    reg [7:0] Led;
     reg [7:0] led_next = 8'b00000000;
     always@(posedge clk)
     	if (reset)
