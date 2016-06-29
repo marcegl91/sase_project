cb6ab0c34a72170278ef18919935120be0713676
tokuwaga <urss_22@hotmail.com>
2016-06-28 23:08:19 -0300

    Update Simulador.cpp
    anadido el modulo shifter para el simulador

--- Ensambladores/Simulador.cpp
+++ Ensambladores/Simulador.cpp
@@ -10,10 +10,17 @@
 #include <stdlib.h>
 #define MAX_SIZE_CODE 128
 #define MAX_CALL 128
-#define DEFAULT_CYCLE 128
+#define DEFAULT_CYCLE 500
 using namespace std;
 //SOPORTE EXPERIMENTAL PARA WINDOWS, para usarlo descomentar la siguiente linea
 //#define Windows_MODE
+
+int reg_0_shifter=0;
+int reg_1_shifter=0;
+int reg_2_shifter=0;
+int reg_3_shifter=0;
+bool registro_modificado=false;
+int wait_time=0;
 
 string int_a_string(int numero){
     string resultado;
@@ -53,7 +60,51 @@ string int_a_binario(int num,int tam_binario){
     }
     return resultado;
 }
-//reensambla
+
+int modulo_in_shifter(int registro){
+    int valor=0;
+    if(registro_modificado){
+        wait_time=0;
+    }
+    switch(registro){
+        case 2: valor=reg_2_shifter;
+        break;
+        case 3:
+                registro_modificado=false;
+                int cant_shift;
+                cant_shift=reg_0_shifter%32768;
+                if(reg_0_shifter>32000) 
+                    reg_2_shifter=reg_1_shifter>>cant_shift;
+                else
+                    reg_2_shifter=reg_1_shifter<<cant_shift;
+                if(wait_time>cant_shift){
+                    valor=0;
+                }
+                else{
+                    wait_time++;
+                    valor=1;
+                }
+        break;
+        default:cout<<"error de lectura del registro shifter"<<endl;
+        break;
+    }
+    return valor;
+}
+
+void modulo_out_shifter(int registro,int valor){
+    registro_modificado=true;
+    reg_3_shifter=1;
+    switch(registro){
+        case 0:reg_0_shifter=valor;
+        break;
+        case 1:reg_1_shifter=valor;
+        break;
+        default:cout<<"error de escritura del registro shifter"<<endl;
+        break;
+    }
+}
+
+
 bool ejecucion(vector<string> &programa,int ciclos){
     
     string line;
@@ -67,21 +118,16 @@ bool ejecucion(vector<string> &programa,int ciclos){
     int call_ejecutado=0;
     for(int ciclo_ejecutado=0;ciclo_ejecutado<ciclos;ciclo_ejecutado++){
         line=programa[pointer];
-        //cout<<line<<endl;
         comando=line.substr(0,2);
         operando1=bin_to_int(line.substr(2,7));
         operando2=bin_to_int(line.substr(9));
-        //cout<<"posiciones: "<<operando1<<" "<<operando2<<endl;
         if(comando=="10"){
             //MOV
-            //cout<<"MOV"<<operando1<<" "<<operando2<<endl;
-            //cout<<programa[operando1]<<" a "<<programa[operando2]<<endl;
             programa[operando2]=programa[operando1];
         }
         else
             if(comando=="00"){
                 //ADD
-                //cout<<"ADD"<<endl;
                 int suma=bin_to_int(programa[operando1])+bin_to_int(programa[operando2]);
                 programa[operando2]=int_a_binario(suma,16);
                 if(suma%65536==0)
@@ -92,7 +138,6 @@ bool ejecucion(vector<string> &programa,int ciclos){
             else
             if(comando=="01"){
                 //CMP
-                //cout<<"CMP"<<endl;
                 if(programa[operando1]==programa[operando2])
                     flag_Z=true;
                 else
@@ -102,12 +147,33 @@ bool ejecucion(vector<string> &programa,int ciclos){
                 comando=line.substr(0,4);
                 if(comando=="1110"){
                     //IN
-                    //No hacer nada
+                    int res=0;
+                    operando1=bin_to_int(line.substr(4,5));
+                    int registro=operando1%4;
+                    int device=(operando1-registro)/4;
+                    //cout<<"IN device: "<<device<<" registro: "<<registro<<endl;
+                    switch(device){
+                        case 2:res=modulo_in_shifter(registro);
+                        break;
+                        default:cout<<"modulo desconectado"<<endl;
+                        break;
+                    }
+                    programa[operando2]=int_a_binario(res,16);
                 }
                 else
                     if(comando=="1111"){
                         //OUT
-                        //No hacer nada
+                        operando1=bin_to_int(line.substr(4,5));
+                        int valor=bin_to_int(programa[operando2]);
+                        int registro=operando1%4;
+                        int device=(operando1-registro)/4;
+                        //cout<<"OUT device: "<<device<<" registro: "<<registro<<endl;
+                        switch(device){
+                            case 2:modulo_out_shifter(registro,valor);
+                            break;
+                            default:cout<<"modulo desconectado"<<endl;
+                            break;
+                        }
                     }
                     else{
                         comando=line.substr(0,9);
