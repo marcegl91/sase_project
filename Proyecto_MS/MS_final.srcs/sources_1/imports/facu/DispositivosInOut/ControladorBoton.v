d5dd0102894b67c9d14fc7bf90f59cc87c239f35
santigl <santix91@gmail.com>
2016-07-06 12:21:39 -0300

    Modificar lógica de lectura botón

--- Proyecto_MS/MS_final.srcs/sources_1/imports/facu/DispositivosInOut/ControladorBoton.v
+++ Proyecto_MS/MS_final.srcs/sources_1/imports/facu/DispositivosInOut/ControladorBoton.v
@@ -1,4 +1,24 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.06.2016 12:17:33
+// Design Name: 
+// Module Name: ControladorBoton
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 
 module ControladorBoton(
     input clk, reset,
@@ -7,27 +27,22 @@ module ControladorBoton(
     output out
     );
     
-    reg btn1;
-    assign out = btn1;
+      
+    wire tick;
+    wire db_level;
+    wire G;
+
+   assign clear=(we)& cs; 
+   assign G= (~we)& (~db_level)& tick;
     
-    reg btn_next =  1'b0;
-    reg btn_reg = 1'b0;
-    
-    always@(posedge clk)
-        begin
-        if (reset)
-            btn1 <= 1'd0;
-        else
-            btn1 <= btn_next;
-        end
-            
-    always@(posedge btn_in, posedge we)
-        if(we)
-            btn_reg <= 1'b0;
-        else
-            btn_reg <= 1'b1;
-            
-    always@*
-        btn_next = btn_reg;        
+   debounce db (.clk(clk), .reset(reset),.sw(btn_in),.db_level(db_level),.db_tick(tick));
+   
+   LDCE LDCE_inst (
+       .Q(out),      // Data output
+       .CLR(clear),  // Asynchronous clear/reset input
+       .D(1'b1),      // Data input
+       .G(G),      // Gate input
+       .GE(cs)     // Gate enable input
+     );       
     
 endmodule
